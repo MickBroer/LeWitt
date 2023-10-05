98fac4bf29854c7cd9b00430f11cee2f237f8f07
Mick Broer <mickbroerxoxo@gmail.com>
2023-10-05 10:09:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let audioBuffers = [];
+let currentSource = null;
+let currentElement = null;  
+let i = 1;
+const title = ["Sol LeWitt ",
+    "Sol LeWit t",
+    "Sol LeWi tt",
+    "Sol LeW itt",
+    "Sol Le Witt",
+    "Sol L eWitt",
+    "Sol  LeWitt",
+    "So l LeWitt",
+    "S ol LeWitt",
+    " Sol LeWitt",
+    "S ol LeWitt",
+    "So l LeWitt",
+    "Sol  LeWitt",
+    "Sol L eWitt",
+    "Sol Le Witt",
+    "Sol LeW itt",
+    "Sol LeWi tt",
+    "Sol LeWit t"
+]
+
+function loadAudioFiles() {
+  const audioFiles = ['01.wav', '02.wav', '03.wav'];
+  
+  audioFiles.forEach((file, index) => {
+    fetch(`audio/${file}`)
+      .then(response => response.arrayBuffer())
+      .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+      .then(audioBuffer => {
+        audioBuffers[index] = audioBuffer;
+      })
+      .catch(error => console.error('Error fetching or decoding audio:', error));
+  });
+}
+
+function animateTitle() {
+    i >= title.length - 1 ? (i = 0) : i++,
+    (document.title = title[i]),
+    setTimeout("animateTitle()", 200);
+    
+}
+
+function playAudio(audioIndex, element) {
+    const innerShape = element.querySelector('.inner-shape');
+    
+    // If the same button is clicked while playing, stop the audio
+    if (currentElement === element && currentSource) {
+      currentSource.stop();
+      currentSource = null;
+      currentElement = null;
+      innerShape.classList.remove('square');
+      innerShape.classList.add('triangle');
+      return;  // Exit the function early
+    }
+  
+    // Reset the previous element to a triangle if a new element is clicked
+    if (currentElement && currentElement !== element) {
+      const previousInnerShape = currentElement.querySelector('.inner-shape');
+      previousInnerShape.classList.remove('square');
+      previousInnerShape.classList.add('triangle');
+    }
+  
+    if (currentSource) {
+      currentSource.stop();
+    }
+  
+    if (audioBuffers[audioIndex]) {
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffers[audioIndex];
+      source.loop = true;  // Loop the audio
+      source.connect(audioContext.destination);
+      audioContext.resume().then(() => {
+        source.start();
+      });
+      currentSource = source;
+      currentElement = element;  // Update the current element
+      innerShape.classList.remove('triangle');
+      innerShape.classList.add('square');
+    } else {
+      console.log(`Audio buffer for index ${audioIndex} is not loaded yet.`);
+    }
+  }
+  
+  
+
+// Load audio files when the page loads
+loadAudioFiles();
+animateTitle();
